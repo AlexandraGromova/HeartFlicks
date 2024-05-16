c23f57f5d86df2cdb8013435a3c61bc360def7a4
Sasha Gromova <sasadanilina2@gmail.com>
2024-05-16 23:35:50 +0600

    first commit

---
+++ HeartFlicks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartFlicks
+//
+//  Created by Sasha on 12.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartFlicks/HeartFlicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartFlicksApp.swift
+//  HeartFlicks
+//
+//  Created by Sasha on 12.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeartFlicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
